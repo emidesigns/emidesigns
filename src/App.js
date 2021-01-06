e490faf5ea4d9725b8b64bcf7b975449ce453b0e
Emilio Diaz <emiliodiaz@Emilios-Mac-mini.local>
2021-01-06 11:46:36 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>emidesings</h1>
+        <div className="">logo</div>
+        <div className="">tagline</div>
+      </header>
+      <h1>emidesings</h1>
+    </div>
+  );
+}
+
+export default App;
